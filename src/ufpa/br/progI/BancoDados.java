f2b619a0ce8b51dfb98a13f96d32f32903ddc931
Marcos Senna <marcosbenaion@gmail.com>
2013-12-17 07:05:03 -0300

    Criando classe abstrata BancoDados

--- src/ufpa/br/progI/BancoDados.java
+++ src/ufpa/br/progI/BancoDados.java
@@ -2,8 +2,15 @@ package ufpa.br.progI;
 
 public abstract class BancoDados 
 {
-	public String nomeBanco;
-	public String tipoBanco;
+	private String nomeBanco;
+	private String tipoBanco;
+	private Data dataCriacao;
+	protected static int qtadeProjetos = 0;
 	
+	public static void verificarProjetos()
+	{
+		System.out.println("A quantidade de projetos existente no Banco de Dados é: "+
+				qtadeProjetos+".\n");
+	}
 
 }
